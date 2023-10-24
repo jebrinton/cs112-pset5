98bcab05017838844d63da59d17d97afb563cae0
jebrinton <122479901+jebrinton@users.noreply.github.com>
2023-10-24 11:44:35 -0400

    working trim

--- StringRecursion.java
+++ StringRecursion.java
@@ -6,8 +6,26 @@ public class StringRecursion {
             printReverse(str.substring(0, str.length() - 1));
         }
     }
+
+    public static String trim(String str) {
+        if (str == null) {
+            throw new IllegalArgumentException();
+        }
+        if (str.equals("")) {
+            return str;
+        }
+
+        if (str.charAt(0) == ' ') {
+            return trim(str.substring(1));
+        }
+        else if (str.charAt(str.length() - 1) == ' ') {
+            return trim(str.substring(0, str.length() - 1));
+        }
+        else {
+            return str;
+        }
+    }
     public static void main(String[] args) {
-        printReverse("Maki");
-        printReverse("Ch i n a");
+
     }
 }
