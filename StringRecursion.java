a521a51cbeee89dc51f4ee8be166921907253ef0
jebrinton <122479901+jebrinton@users.noreply.github.com>
2023-10-24 11:16:50 -0400

    working printReverse

--- StringRecursion.java
+++ StringRecursion.java
@@ -1,5 +1,13 @@
 public class StringRecursion {
-    public static void main(String[] args) {
 
+    public static void printReverse(String str) {
+        if (str != null && str.length() != 0) {
+            System.out.print(str.charAt(str.length() - 1));
+            printReverse(str.substring(0, str.length() - 1));
+        }
+    }
+    public static void main(String[] args) {
+        printReverse("Maki");
+        printReverse("Ch i n a");
     }
 }
